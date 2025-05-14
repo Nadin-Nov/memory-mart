d92949670cd2ce5c468ac063945edd774eafa96d
alvorie <123foggy123@gmail.com>
2025-05-14 15:23:06 +0300

    fix: make external link and type optional for primary button component

--- src/components/PrimaryButton/PrimaryButton.tsx
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -1,22 +1,43 @@
-import type { ReactElement } from 'react';
 import { Button } from '@chakra-ui/react';
+import type { ReactElement } from 'react';
 
 export interface ButtonProps {
   title: string;
-  link: string;
+  externalLink?: string;
+  type?: 'button' | 'reset' | 'submit';
   onClick?: (event: React.FormEvent) => void;
 }
 
-export const PrimaryButton = ({ title, link, onClick }: ButtonProps): ReactElement => {
+export const PrimaryButton = ({ title, externalLink, type = 'button', onClick }: ButtonProps): ReactElement => {
   const primaryButtonHandler = (event: React.FormEvent): void => {
     if (onClick) {
       event.preventDefault();
       onClick(event);
     }
   };
+
+  if (externalLink) {
+    return (
+      <a href={externalLink}>
+        <Button
+          type={type}
+          borderRadius={10}
+          width='full'
+          colorPalette='teal'
+          paddingX='20'
+          paddingY='4'
+          onClick={primaryButtonHandler}
+          asChild
+        >
+          {title}
+        </Button>
+      </a>
+    );
+  }
+
   return (
     <Button
-      type='submit'
+      type={type}
       borderRadius={10}
       width='full'
       colorPalette='teal'
@@ -25,7 +46,7 @@ export const PrimaryButton = ({ title, link, onClick }: ButtonProps): ReactEleme
       onClick={primaryButtonHandler}
       asChild
     >
-      <a href={link}>{title}</a>
+      {title}
     </Button>
   );
 };
