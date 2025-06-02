eaec118ae026c0dc6850b7626e7170acc5c52757
Gnarkill33 <gnarkill33@gmail.com>
2025-06-03 02:12:44 +0300

    fix: modify navigate feature

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -6,6 +6,7 @@ import PersonalInfo from '@/components/UserDetails/PersonalInfo';
 import { getCustomerDetails } from '@/services/CustomerService';
 import { useAuth } from '@/context/useAuth';
 import PasswordInfo from '@/components/UserDetails/PasswordInfo';
+import { useNavigate } from 'react-router-dom';
 
 const UserProfilePage = (): ReactElement => {
   const initialCustomerDetails = {
@@ -20,7 +21,17 @@ const UserProfilePage = (): ReactElement => {
     addresses: [],
   };
 
-  const { userData } = useAuth();
+  const navigate = useNavigate();
+
+  const { userData, isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      return;
+    } else {
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
 
   const [customerDetails, setCustomerDetails] = useState<CustomerDetailsTypeWithToken>(initialCustomerDetails);
 
